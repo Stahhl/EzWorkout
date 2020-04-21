a2242ca61d12b02535a1836ac0efe3b6a165df28
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2020-04-21 22:28:31 +0200

    dummy workouts

--- EzWorkout/EzWorkout/ViewModels/BrowseWorkoutsViewModel.cs
+++ EzWorkout/EzWorkout/ViewModels/BrowseWorkoutsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EzWorkout.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
@@ -9,7 +10,16 @@ namespace EzWorkout.ViewModels
     {
         public BrowseWorkoutsViewModel()
         {
+            generateDummyData();
+        }
 
+        private void generateDummyData()
+        {
+            workouts = new ObservableCollection<WorkoutViewModel>();
+
+            workouts.Add(new WorkoutViewModel() { Workout = new Workout() { Name = "New Workout 1" }, Intervals = new ObservableCollection<Interval>() });
+            workouts.Add(new WorkoutViewModel() { Workout = new Workout() { Name = "New Workout 2" }, Intervals = new ObservableCollection<Interval>() });
+            workouts.Add(new WorkoutViewModel() { Workout = new Workout() { Name = "New Workout 3" }, Intervals = new ObservableCollection<Interval>() });
         }
 
         private ObservableCollection<WorkoutViewModel> workouts;
